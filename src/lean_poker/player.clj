69818476b28b79438490545a54c56dbb8dfe5c08
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-11-26 13:59:19 +0300

    Add strategy

--- src/lean_poker/player.clj
+++ src/lean_poker/player.clj
@@ -2,15 +2,15 @@
   (:require [taoensso.timbre :as log]
             [lean-poker.hand :as hand]))
 
-(def version "0.0.9-snapshot")
+(def version "0.0.10-snapshot")
 
 (def small_bet 0)
 (def big_bet 230)
 
 (defn check-hand [a b]
   (cond
-    (and (> a 9) (> b 9) (= a b)) true
-    (= a b) true
+    (and (> a 8) (> b 8) (= a b)) true
+    (and (> a 10) (> b 10)) true
     :else false))
 
 (defn rank-weight [{rank :rank}]
