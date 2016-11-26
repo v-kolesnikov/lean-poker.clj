4858df28d01459b44040b9523f0870c2370e77bc
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-11-26 12:08:50 +0300

    Add log game-state

--- src/lean_poker/player.clj
+++ src/lean_poker/player.clj
@@ -1,11 +1,14 @@
-(ns lean-poker.player)
+(ns lean-poker.player
+  (:require [taoensso.timbre :as log]))
 
 (def version "Clojure-y folding player")
 
 (defn bet-request
   [game-state]
+  (log game-state)
   0)
 
 (defn showdown
   [game-state]
+  (log game-state)
   nil)
