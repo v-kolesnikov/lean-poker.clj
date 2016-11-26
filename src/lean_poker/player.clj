460ce53adc925a7e2db793f7b93dbae1d27ff149
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-11-26 14:55:45 +0300

    Add new strategy

--- src/lean_poker/player.clj
+++ src/lean_poker/player.clj
@@ -4,8 +4,20 @@
 
 (def version "0.0.13-lucky")
 
-(def small_bet 0)
-(def big_bet 230)
+(def small-bet 0)
+(def mid-bet 100)
+(def big-bet 230)
+
+(defn check-state [a b x y z]
+  (let []
+    (cond
+          (and (> a 8) (> b 8) (= a b)) big-bet
+          (and (> a 10) (> b 10)) big-bet
+          (or (= a x) (= a y)
+              (= a z) (= b x)
+              (= b y) (= b z)) mid-bet
+          :else small-bet)
+    ))
 
 (defn check-hand [a b]
   (log/info "In check-hand: a:" a)
@@ -31,20 +43,18 @@
         "K" 12
         "A" 13} rank))
 
-(def small_bet 0)
-(def big_bet 230)
-
 (defn bet-request
   [game-state]
   (log/info (str "[GAME STATE line 37] " game-state))
   (try
     (let [card1 (first  (hand/my-cards game-state))
           card2 (second (hand/my-cards game-state))]
-      (if (check-hand (rank-weight card1) (rank-weight card2))
-        big_bet
-        small_bet))
+      (->> hand/visible-cards
+         (map rank-weight)
+         (check-state)))
     (catch Exception e
       small_bet)))
+
 
 
 (defn showdown
