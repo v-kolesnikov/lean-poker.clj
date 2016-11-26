1c7c04bd4b5e7361972e5c92eb71667b7fc8923c
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-11-26 12:21:28 +0300

    Add bet is 100

--- src/lean_poker/player.clj
+++ src/lean_poker/player.clj
@@ -1,12 +1,12 @@
 (ns lean-poker.player
   (:require [taoensso.timbre :as log]))
 
-(def version "0.0.3-snapshot")
+(def version "0.0.4-snapshot")
 
 (defn bet-request
   [game-state]
   (log/info game-state)
-  0)
+  100)
 
 (defn showdown
   [game-state]
